3c0d90c458a33df54ba9910d8a07e7b98568d823
Celine Ha <celineha6@gmail.com>
2024-07-03 16:03:39 -0700

    test commit

---
+++ ControlPanel.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+package javiergs.gui.paint.gamma;
+
+import javax.swing.*;
+
+public class ControlPanel {
+
+    /**
+     * StatusPanel creates a panel with two buttons: Undo and Redo.
+     * This version includes an ActionListener.
+     *
+     * Author: javiergs
+     * Version: 2.0
+     */
+    public class StatusPanel extends JPanel {
+
+        private JButton undoButton;
+        private JButton redoButton;
+
+        public StatusPanel() {
+            undoButton = new JButton("Undo");
+            redoButton = new JButton("Redo");
+            add(undoButton);
+            add(redoButton);
+
+            ActionNanny actionNanny = new ActionNanny();
+            undoButton.addActionListener(actionNanny);
+            redoButton.addActionListener(actionNanny);
+        }
+
+        // Method to return the undo button
+        public JButton getUndoButton() {
+            return undoButton;
+        }
+
+        // Method to return the redo button
+        public JButton getRedoButton() {
+            return redoButton;
+        }
+    }
+
+}
