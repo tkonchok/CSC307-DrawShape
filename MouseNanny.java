44b418b837ba5e52089c600b2135349d7a2b5dd3
Celine Ha <51255754+celineha6@users.noreply.github.com>
2024-07-03 16:47:23 -0700

    Update MouseNanny.java

--- MouseNanny.java
+++ MouseNanny.java
@@ -4,36 +4,43 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 /**
- * MouseNanny listens for mouse events.
- * Send information to Officer.
+ * MouseNanny listens for mouse events to facilitate drawing operations.
+ * Sends information to Officer for handling drawing actions.
  *
  * @author javiergs
- * @version 2.0
+ * @version 1.0
  */
 public class MouseNanny implements MouseListener {
-	
+
+	@Override
 	public void mouseClicked(MouseEvent e) {
+		// Not used in this implementation
 	}
-	
-	public void mouseEntered(MouseEvent e) {
-	}
-	
-	public void mouseExited(MouseEvent e) {
-	}
-	
+
+	@Override
 	public void mousePressed(MouseEvent e) {
 		int x = e.getX();
 		int y = e.getY();
 		Officer.setX(x);
 		Officer.setY(y);
 	}
-	
+
+	@Override
 	public void mouseReleased(MouseEvent e) {
 		int x = e.getX();
 		int y = e.getY();
 		Officer.setWidth(x - Officer.getX());
 		Officer.setHeight(y - Officer.getY());
-		Officer.tellYourBoss();
+		Officer.performDrawAction(); // Perform drawing action on mouse release
 	}
-	
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// Not used in this implementation
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// Not used in this implementation
+	}
 }
